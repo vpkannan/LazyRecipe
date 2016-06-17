de7bb2e0af1267ae4038df80cc2d14723e017275
Vignesh <vignesh.p.kannan@gmail.com>
2016-06-16 21:03:03 -0500

    Parameterizing ajax query

--- src/main/webapp/getRecipeByName.js
+++ src/main/webapp/getRecipeByName.js
@@ -8,7 +8,11 @@ $(document)
 					$
 							.ajax(
 									{
-										url : "http://ec2-52-39-75-111.us-west-2.compute.amazonaws.com:8080/lazyrecipe/rest/recipe/get?dishname=Plain%20Dosa"
+										url : "http://ec2-52-39-75-111.us-west-2.compute.amazonaws.com:8080/lazyrecipe/rest/recipe/get",
+										type : "get",
+										data : {
+											dishname : "Omlet"
+										}
 									}).then(function(data) {
 								$('.dish-name').append(data.dishName);
 								$('.dish-bestfor').append(data.bestFor);
