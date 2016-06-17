1393ecc43efedf3d1b8d1898022590d3239c8895
Vignesh <vignesh.p.kannan@gmail.com>
2016-06-16 21:14:16 -0500

    Pass form data from UI as query to REST API

--- src/main/webapp/getRecipeByName.js
+++ src/main/webapp/getRecipeByName.js
@@ -2,19 +2,21 @@
  * 
  */
 
-$(document)
-		.ready(
-				function() {
-					$
-							.ajax(
-									{
-										url : "http://ec2-52-39-75-111.us-west-2.compute.amazonaws.com:8080/lazyrecipe/rest/recipe/get",
-										type : "get",
-										data : {
-											dishname : "Omlet"
-										}
-									}).then(function(data) {
-								$('.dish-name').append(data.dishName);
-								$('.dish-bestfor').append(data.bestFor);
-							});
-				});
+$(document).ready(function() {
+	$('#dishName').change(function() {
+		getRecipeByName($('#dishName').val());
+	});
+});
+
+function getRecipeByName(dishName) {
+	$.ajax({
+		url : "http://ec2-52-39-75-111.us-west-2.compute.amazonaws.com:8080/lazyrecipe/rest/recipe/get",
+		type : "get",
+		data : {
+			dishname : "Omlet"
+		}
+	}).then(function(data) {
+		$('.dish-name').append(data.dishName);
+		$('.dish-bestfor').append(data.bestFor);
+	});
+}
